231dbf8cda03c21784707fdff48950c8f2cb94aa
psakharov <s.p.alekseevich@gmail.com>
2025-09-02 18:46:28 +0300

    change starter kit: vue -> livewire

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
-import vue from '@vitejs/plugin-vue';
+import {
+    defineConfig
+} from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
             refresh: true,
         }),
         tailwindcss(),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
     ],
+    server: {
+        cors: true,
+    },
 });
