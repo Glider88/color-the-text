d93b66528daaa8b03f10ef8e913b5c44fb65616a
psakharov <s.p.alekseevich@gmail.com>
2025-11-26 17:43:41 +0300

    remove lab

--- routes/lab.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-
-use App\Http\Controllers\LabController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/lab/count', [LabController::class, 'count']);
-Route::get('/lab', [LabController::class, 'lab']);
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,3 @@
 <?php declare(strict_types=1);
 
-require __DIR__.'/lab.php';
 require __DIR__.'/color.php';
